cd529024d716261ed4748354b1e55665bd4852ef
Medha <medha.ranga@yahoo.com>
2022-11-17 16:07:02 +0000

    Updated with comments

--- proj4.R
+++ proj4.R
@@ -15,22 +15,23 @@
 newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
               fscale=1,maxit=100,max.half=20,eps=1e-6)
 { 
-  #Arguements
+  #Arguments
   
-  # theta : vector of initial values for the optimization parameters.
+  #theta : vector of initial values for the optimization parameters.
   
   #func   : The objective function to minimize. Its first argument is the vector 
   #         of optimization parameters. Remaining arguments are passed from newt 
   #         using '...'
   
   #grad   : The gradient function. It has the same arguments as func but returns 
-  #         the gradient vector of the objective w.r.t. the elements of parameter 
-  #         vector
+  #         the gradient vector of the objective w.r.t. the elements of  
+  #         parameter vector
   
   #hess   : the Hessian matrix function. It has the same arguments as func but 
   #         returns the Hessian matrix of the objective w.r.t. the elements of 
   #         parameter vector. If not supplied then newt should obtain an 
-  #         approximation to the Hessian by finite differencing of the gradient vector
+  #         approximation to the Hessian by finite differencing of the gradient 
+  #         vector 
   #         Default : NULL
   
   #tol    : convergence tolerance. Default = 1e-8
@@ -41,24 +42,25 @@ newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
   #maxit  : the maximum number of Newton iterations to try before giving up.
   #         Default = 100
   
-  #maxit  : the maximum number of times a step should be halved before concluding
-  #         that the step has failed to improve the objective. Default = 20
+  #maxit  : the maximum number of times a step should be halved before 
+  #         concluding that the step has failed to improve the objective. 
+  #         Default = 20
   
-  #eps    : the finite difference intervals to use when a Hessian function is not provided
+  #eps    : the finite difference intervals to use when a Hessian function is 
+  #         not provided 
   #         Default: 1e-6
   
   #Returns
   # x     : a list containing
-  #       f     : value of the objective function at the minimum.
-  #       theta : the value of the parameters at the minimum
-  #       iter  : the number of iterations taken to reach minimum
-  #       g     : the gradient vector at the minimum
-  #       Hi    : the inverse of the hessian matrix at the minimum
-  
+  # f     : value of the objective function at the minimum.
+  # theta : the value of the parameters at the minimum
+  # iter  : the number of iterations taken to reach minimum
+  # g     : the gradient vector at the minimum
+  # Hi    : the inverse of the hessian matrix at the minimum
   
   
   #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  #checks if the objective are not finite at the initial theta;
+  #checks if the objectives are not finite at the initial theta;
   if(is.infinite(func(theta,...)))
   {
     stop("objective not finite at the initial theta")
@@ -71,9 +73,25 @@ newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
   }
   #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   
-  #function to form Hessian Matrix
+  #Function to create a hessian matrix using finite difference approximation
   hess_created=function(theta,grad,...)
   {
+  # First, we calculate gradient with initial theta and then looping over 
+  # every element of theta, we increase it by eps and find new gradient. 
+  # Finally, by differencing old gradient from the new gradient and dividing  
+  # with eps, we get our hessian matrix. 
+    
+  #Arguments
+    
+  #theta  : vector of values of optimization parameters.
+    
+  #grad   : The gradient function. It has the same arguments as func but returns 
+  #         the gradient vector of the objective w.r.t. the elements of  
+  #         parameter vector
+    
+  #Returns
+  #Hfd    : Hessian matrix 
+    
     hb1 <- grad(theta,...)             #grad at theta
     Hfd <- matrix(0,length(theta),length(theta))     #finite difference Hessian
     for (i in 1:length(theta))      #loop over parameters
@@ -88,48 +106,89 @@ newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
   
   #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   
-  #function to check convergence
-  conv_grad=function(theta,grad,func,tol,fscale,...)
+  #Function to check convergence
+  conv_grad = function(theta,grad,func,tol,fscale,...)
   {
-    gradient=grad(theta,...)   #forms gradient
-    for(ele in gradient)   #loop over elements of gradient
+  # We first form the gradient vector using 'grad' function for theta 
+  # Then, iterating over every element of gradient, we check if absolute value 
+  # of element is less than tolerance times absolute value of the objective 
+  # function plus fscale. If Yes, it returns "True" otherwise it returns "False" 
+  # and breaks the loop.
+    
+  #Arguments
+  
+  #theta  : vector of values of optimization parameters.
+    
+  #grad   : The gradient function. It has the same arguments as func but returns 
+  #         the gradient vector of the objective w.r.t. the elements of 
+  #         parameter vector
+  
+  #func   : The objective function to minimize. Its first argument is the vector 
+  #         of optimization parameters. Remaining arguments are passed from newt 
+  #         using '...'
+  
+  #tol    : convergence tolerance. Default = 1e-8
+  
+  #fscale : a rough estimate of the magnitude of func near the optimum - 
+  #         used in convergence testing.Default = 1  
+  
+  #Returns
+  #result : True or False
+ 
+    gradient=grad(theta,...)   #forms gradient vector
+    for(ele in gradient)       #loop over elements of gradient
     {
       if(abs(ele)< ((fscale + abs(func(theta,...)))*tol))  #checks convergence
       { result=TRUE}     
       else
       {result=FALSE
       break}
-      
     }
     return(result)    #returns result
   }
   
-  pos_func=function(mat2)
+  #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+  
+  pos_func = function(matrix)
   { 
-    identity_mat = diag(NCOL(mat2))  #gets identity matrix
-    i=1e-6     #lamda value intitiated to 1
-    # res will gives error if matrix is not positive definite
-    res <- try(chol(mat2),silent = TRUE) 
+  # First, we create Identity matrix of given matrix size. Then, considering
+  # multiplier as 1e-6, we check if matrix is positive definite. If it is 
+  # positive definite, it returns the matrix. Otherwise, it adds a multiple of 
+  # the identity matrix until it's positive definite where we increase the 
+  # multiplier 10 times and returns New matrix (Positive definite)
+  
+  
+  #Arguments
+  #matrix            : Hessian matrix  
+   
+  #Returns
+  #new_matrix/matrix : Positive definite matrix
+  
+    identity_mat = diag(NCOL(matrix))  #gets identity matrix
+    multiplier=1e-6                    #multiplier value intitiated to 1e-6
+    # res will give error if matrix is not positive definite
+    res <- try(chol(matrix),silent = TRUE) 
     if(any(class(res)!="try-error")) #checks if res gives error
     {
-      return(mat2)   #if not it returns original matrix
+      return(matrix)   #if not it returns original/positive definite matrix
     }
     else   #if it gives error--> not positive definite
     {
       while (any(class(res)=="try-error"))  #loops until its positive definite
       { 
-        mat2=(mat2+t(mat2))/2
-        mat_2=mat2+i*identity_mat    #adds a multiple of the identity matrix
-        i=i*10                       #increase i(lamda) by 1
-        res <- try(chol(mat_2),silent = TRUE)  #computes result
+        matrix=(matrix+t(matrix))/2  # converts into symmetric matrix
+        # adds a multiple of the identity matrix
+        new_matrix = matrix+multiplier*identity_mat    
+        multiplier = multiplier*10      #increase multiplier 10 times
+        res <- try(chol(new_matrix),silent = TRUE)  #computes result
       }
-      return(mat_2)  #returns positive definite matrix
+      return(new_matrix)  #returns positive definite matrix
     }
   }
     
   #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  iter=0   #intialize inter=1
+  iter=0   #initialize iter = 0
   
   while(iter<=maxit)       #while loop till maxit
   { 
@@ -140,27 +199,33 @@ newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
     #checks convergence
     if(conv_test_val==FALSE)    #if convergence not reached
     { 
-      if(iter==maxit)
+      if(iter==maxit)   # checks if maxit is reached
       {stop("maxit is reached without convergence")}
       
       else
     {
       obj_old=func(theta,...)       #obj function value for theta
       gradient=grad(theta,...)      #gradient for theta
-      #computes hessian using hess().using pos_func(),it checks if its positive  
-      #definite or not if not it converts and returns positive definite hessian
-      if(is.null(hess)){Hessian=pos_func(hess_created(theta,grad,...))}
-      else{Hessian=pos_func(hess(theta,...))}
       
-      Hi=chol2inv(chol(Hessian))
-      delta=-Hi%*%gradient  #computes delta(stepsize)
+      #computes hessian using hess_created() if hess function is not given.
+      #Then, using pos_func(), it checks if hessian is positive or not. If it is
+      #not, we get a positive definite hessian. 
+    
+      if(is.null(hess)){Hessian=pos_func(hess_created(theta,grad,...))}
+      else{Hessian = pos_func(hess(theta,...))}
+      
+      Hi=chol2inv(chol(Hessian)) # Get inverse of hessian matrix.
+      delta = -Hi%*%gradient  #computes delta
       
       new_theta=theta+delta    #computes new theta
-      obj_new=func(new_theta)  #computes new obj function value
+      obj_new=func(new_theta)  #computes new objective function value
       
-      no_step_half=0     #number of steps half
-      while(obj_old<=obj_new || is.infinite(obj_new) )  #iterate till obj_new is 
-                                                        #less than obj_old
+      no_step_half = 0    # Initialize the number of step half
+      
+      # Checks if Delta overshoots and increases the objective function or 
+      # objective function is infinite.
+      
+      while(obj_old<=obj_new || is.infinite(obj_new) )  
       {
         if(no_step_half<=max.half)   #checks if no_step_half exceeds max.half 
         {
@@ -169,21 +234,21 @@ newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
           obj_new=func(new_theta,...)  #obj_new after reducing
           no_step_half=no_step_half+1   #no_step_half increased by 1
         }
-        else #if no_step_half exceeds max.half we get warning
+        else #if no_step_half exceeds max.half, function stops with error.
         {  
           stop("the step fails to reduce the objective despite trying
                   max.half step halvings")
         }
       }
-      theta=new_theta
+      theta=new_theta   # initialize theta as new_theta.
       
       }}
     #**************************************************
-    else #if convergence reached
+    else #if convergence is reached
     { 
       g=grad(theta,...)   #optimal gradient
       f=func(theta,...)   #optimal obj function
-      iter=iter       # number of iteration
+      iter=iter       # number of iterations
       
       #computes hessian
       if(is.null(hess)){Hessian=hess_created(theta,grad,...)}
@@ -196,16 +261,15 @@ newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
       if(any(class(res)=="try-error"))  
         {
           stop("Hessian matrix is not finte at convergence")
-          #x=list(f=f,theta=theta,iter=iter,g=g)  # list to be return
         }
       else
         {
-          Hi=chol2inv(chol(Hessian))
-          x=list(f=f,theta=theta,iter=iter,g=g,Hi=Hi)  # list to be return
+          Hi=chol2inv(chol(Hessian)) # computes inverse of Hessian
+          x=list(f=f,theta=theta,iter=iter,g=g,Hi=Hi)  # list to be returned
           return(x)
         }
         
-      break  #once convergence find out it breaks the loop
+      break  #once convergence is found, it breaks the loop
     } 
     
     iter=iter+1  #iter increased by 1
@@ -213,6 +277,7 @@ newt=function(theta,func,grad,hess=NULL,...,tol=1e-8,
   
 }
 
+###############################################################################
 ###############################################################################
 #objective function
 
@@ -230,7 +295,9 @@ gb <- function(th,k=2) {
 
 ###############################################################################
 ###############################################################################
+
 #hessian matrix
+
 hb <- function(th,k=2) {
   h <- matrix(0,2,2)
   h[1,1] <- 2-k*2*(2*(th[2]-th[1]**2) - 4*th[1]**2)
@@ -239,8 +306,11 @@ hb <- function(th,k=2) {
   h
 }
 
-newt(c(12,35),rb,gb,k=1)
-newt(c(12,35),rb,gb,hb,k=1)
+###############################################################################
+###############################################################################
+
+newt(c(0,3),rb,gb,k=1)
+newt(c(0,3),rb,gb,hb,k=1)
 
 newt(c(1000,7),rb,gb)
 newt(c(0,1),rb,gb,hb)
